667f1df73c7d588ffec4379f7d15e69b2e9987ef
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-07-01 01:22:40 +0000

    aa

--- models/amenity.py
+++ models/amenity.py
@@ -3,6 +3,6 @@
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
+class Amenity(BaseModel):
     """Amenity"""
     name = ""
--- models/review.py
+++ models/review.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
+class Review(BaseModel):
     """review"""
     place_id = ""
     user_id = ""
